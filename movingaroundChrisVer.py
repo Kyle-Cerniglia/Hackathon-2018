1f8c233cb6bd54257159f455860f8beb7fd5fd17
Chris Backhaus <christophereback@gmail.com>
2018-01-14 04:25:27 -0700

    updating my python

--- movingaroundChrisVer.py
+++ movingaroundChrisVer.py
@@ -2,25 +2,53 @@ import serial
 import picamera
 import time
 
+def encounterObj(objectColor):
+	#assigns the distance the robot should stay around the obstacle
+	if (objectColor == Red):
+		radius = 3
+	else:
+		radius = 1
+
+	port.write("~2632")
+
+	while (angle < 90):
+		#repeats until on the opposite side of object
+		dist = sensor4Data
+		while(dist >= radius + 0.2 or dist <= radius - 0.2):
+			readSerial()
+			dist = sensor4Data
+			port.write("~6021")
+		if (dist < radius):
+			#turn 5 degrees right by running left motor forward
+			port.write("~1052")
+		else:
+			#turn 5 degrees left by running right motor forward
+			port.write("~4052")
+			
+
+	port.write("~1902")
+
+
 def readSerial():
 	port.write("~9000")
 	if(port.in_waiting >= 20):
 		inputData = str(port.read(31))
 		print(inputData)
 		dataList = inputData.split(",")
-		sensor1Data = dataList[1]
-		sensor2Data = dataList[2]
-		sensor3Data = dataList[3]
-		sensor4Data = dataList[4]
+		global sensor1Data = dataList[1]
+		global sensor2Data = dataList[2]
+		global sensor3Data = dataList[3]
+		global sensor4Data = dataList[4]
 		heading = dataList[5]
 		if (sensor1Data <= 40):#this compares sensor value to see if object is within 4 inches
 			sensor1 = 1
 		if (sensor2Data <= 40):
-			sensor2 = 1
+			ensor2 = 1
 		if (sensor3Data <= 40):
 			sensor3 = 1
 		if (sensor4Data <= 40):
-			sensor4 = 1
+			sensor4 = 1		
+
 
 port = serial.Serial("/dev/ttyAMA0",baudrate=9600,timeout=3.0)
 #sensor1 is middle front sensor
@@ -29,18 +57,18 @@ port = serial.Serial("/dev/ttyAMA0",baudrate=9600,timeout=3.0)
 #sensor4 is rear sensor
 
 seeGreen = 0
-sensor1 = 0
-sensor2 = 0
-sensor3 = 0
-sensor4 = 0
+global sensor1 = 0
+global sensor2 = 0
+global sensor3 = 0
+global sensor4 = 0
 
 while True:
 	readSerial()
-	port.write("~6021")
+	port.write("~6201")
 	time.sleep(1)
 	port.write("~8000")
 #camera = picamera.PiCamera()
-'''
+
 for x in range(0,6): #does an inital sweep of surroundings looking for green objective
 	#camera.capture('image.jpg')
 	#objectType = identifyImage('image.jpg')
@@ -54,17 +82,16 @@ while goal != 1:
 		port.write("left 60")#left 60
 		readSerial()
 		if(sensor1 == 1 or sensor2 == 1 or sensor3 == 1):
+			encounterObj(objectType)
 			sensor1 = 0
 			sensor2 = 0
 			sensor3 = 0
 			sensor4 = 0
-			port.write("right 63")#right 63
 	while seeGreen == 1:
 		port.write("forward")#forward
 		if(sensor1 == 1 or sensor2 == 1 or sensor3 == 1):
+			encounterObj(objectType)
 			sensor1 = 0
 			sensor2 = 0
 			sensor3 = 0
 			sensor4 = 0
-			port.write("right 63")#right 63
-'''
